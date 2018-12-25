afdf8e31bfc36e1ac728f173b76c4f925f6ad501
Julian K. Arni <julian@monadic.xyz>
2018-12-25 19:23:15 +0100

    Start frontend stuff.

--- shell.nix
+++ shell.nix
@@ -1,20 +1,22 @@
 { pkgs ? import <nixpkgs> {}
 , compiler ? "ghc843"
-, doc ? true
+, frontend ? true
 , extras ? true
 }:
 
 with pkgs;
-
+with ocamlPackages_4_02;
 
 stdenv.mkDerivation {
     name = "netizens-dev";
     buildInputs = [ ghc zlib glibcLocales wget stack postgresql ]
+      ++ (if frontend then [ nodejs-8_x ocaml ninja merlin ] else [])
       ++ (if extras then [ vimPlugins.stylish-haskell haskellPackages.apply-refact hlint ] else []);
     LANG = "en_US.UTF-8";
     libraryPkgconfigDepends = [ zlib ];
     shellHook = ''
       export PATH=$PATH:`stack path --local-bin`
+      export PATH="`pwd`/static/node_modules/.bin:$PATH"
       export STACK_ARGS="--system-ghc --no-nix-pure --silent --nix-packages 'zlib'"
       export IS_NIX_SHELL="true"
       export LOCALE_ARCHIVE="${pkgs.glibcLocales}/lib/locale/locale-archive";
---
+++ static/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  entry: {
+    app: './src/App.bs.js',
+  },
+  mode: isProd ? 'production' : 'development',
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: '[name].js',
+  },
+};
